0d993bbba7013b03b1fa9ca4d8fb4b5806298e8c
nastya13 <nastya94-94.08@mail.ru>
2015-02-21 12:34:09 +0300

    adding src-folder

---
+++ src/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+--- Fibonachi: 1 -> 0; 2 -> 1; 3 -> 1; 4 -> 2; 5 -> 3 ... 
+--- if < 0 then -1 "error"
+fib :: Int -> Int
+fib a =
+  if a < 0
+    then - 1
+    else if a < 2
+           then 0
+           else
+             let
+               stop p = p < 4 
+
+               go bef1 bef2 p = if stop p
+                                  then (bef1 + bef2)
+                                  else let bef1' = bef2
+                                           bef2' = bef1 + bef2
+                                           p' = p - 1
+                                       in go bef1' bef2' p'
+             in go 0 1 a
+
+main = print (fib 9)
